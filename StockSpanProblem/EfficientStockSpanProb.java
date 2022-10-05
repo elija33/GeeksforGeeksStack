e4bc3ceb2efa00bcdd92d22de64fa3ab19d14c71
Elija33 <amponsaeli@gmail.com>
2022-10-05 08:19:20 -0400

    Stock Span Problem

--- StockSpanProblem/EfficientStockSpanProb.java
+++ StockSpanProblem/EfficientStockSpanProb.java
@@ -1,11 +1,24 @@
 package StockSpanProblem;
 
-public class EfficientStockSpanProb {
-    public void printSpan(int number[]) {
+import java.util.Stack;
 
+public class EfficientStockSpanProb {
+    public static void printSpan(int number[]) {
+        Stack<Integer> name = new Stack<>();
+        name.add(0);
+        System.out.print(1 + " ");
+        for (int i = 1; i < number.length; i++) {
+            while (name.isEmpty() == false && number[name.peek()] <= number[i]) {
+                name.pop();
+            }
+            int span = name.isEmpty() ? i + 1 : i - name.peek();
+            System.out.print(span + " ");
+            name.push(i);
+        }
     }
 
     public static void main(String[] args) {
-
+        int number[] = new int[] { 18, 12, 13, 14, 11, 16 };
+        printSpan(number);
     }
 }
